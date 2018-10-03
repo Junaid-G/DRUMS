276e4436c55797f85959a5590155d7262884f4cc
Junaid-G <43418082+Junaid-G@users.noreply.github.com>
2018-10-03 19:07:24 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+console.log('hello')
+let keyAElement = document.getElementById("key-A")
+
+let audioAElement = document.getElementById("audio-A")
+
+keyAElement.onclick = () => {
+    console.log('man like')
+    audioAElement.play()
+}
+
+let keySElement = document.getElementById("key-S")
+
+let audioSElement = document.getElementById("audio-S")
+
+keySElement.onclick = () => {
+    console.log('man like')
+    audioSElement.play()
+}
+
+let keyDElement = document.getElementById("key-D")
+
+let audioDElement = document.getElementById("audio-D")
+
+keyDElement.onclick = () => {
+    console.log('man like')
+    audioDElement.play()
+}
